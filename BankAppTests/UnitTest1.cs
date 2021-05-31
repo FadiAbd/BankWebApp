a694bfddd025927ede6e2bf3c349513356bbebaf
FadiAbd <fadishadow@hotmail.com>
2021-05-31 23:23:57 +0200

    MSTest Project

---
+++ BankAppTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankAppTests
+{
+    [TestClass]
+    public class WithdrawalControllerTests
+    {
+        private WithdrawalController sut = new WithdrawalController();
+        [TestMethod]
+        public void Should-not-withdraw-more-than-the-Account-totalAmount  ()
+        {
+            //
+        }
+    }
+}
---
+++ BankWebApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankWebApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankWebApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a772d6c-cbbf-4d8d-978f-35fc87779cfb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
