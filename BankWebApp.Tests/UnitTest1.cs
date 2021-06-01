212df005581a567bdc5029fbf57d7f08c9c08e73
FadiAbd <fadishadow@hotmail.com>
2021-06-01 22:35:24 +0200

    tester

--- BankWebApp.Tests/UnitTest1.cs
+++ BankWebApp.Tests/UnitTest1.cs
@@ -1,10 +1,28 @@
+using BankWebbApp.Controllers;
+using BankWebbApp.Data;
+using BankWebbApp.Repository;
+using BankWebbApp.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace BankWebApp.Tests
 {
-    [TestClass]
-    public class UnitTest1
+    public class BaseTest
     {
+        protected AutoFixture.Fixture fixture = new AutoFixture.Fixture();
+        private ApplicationDbContext ctx;
+
+    }
+        [TestClass]
+    public class WithdrawalControllerTests : BaseTest
+    {
+
+        private WithdrawalController sut;
+        private Mock<ITransactionRepository> transactionRepositoryMock;
+        private Mock<IAccountRepository> accountRepositoryMock;
+        private KrisInfo info;
+        private ApplicationDbContext ctx;
+
         [TestMethod]
         public void TestMethod1()
         {
