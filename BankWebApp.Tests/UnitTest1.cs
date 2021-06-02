341601425a3cb13e6bc84d2851c090bf229f718d
FadiAbd <fadishadow@hotmail.com>
2021-06-02 23:44:29 +0200

    tester3

--- BankWebApp.Tests/UnitTest1.cs
+++ BankWebApp.Tests/UnitTest1.cs
@@ -2,8 +2,11 @@ using BankWebbApp.Controllers;
 using BankWebbApp.Data;
 using BankWebbApp.Repository;
 using BankWebbApp.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
+using System.IO;
 
 namespace BankWebApp.Tests
 {
@@ -20,12 +23,28 @@ namespace BankWebApp.Tests
         private WithdrawalController sut;
         private Mock<ITransactionRepository> transactionRepositoryMock;
         private Mock<IAccountRepository> accountRepositoryMock;
-        private KrisInfo info;
         private ApplicationDbContext ctx;
 
-        [TestMethod]
-        public void TestMethod1()
+        public WithdrawalControllerTests()
         {
+            transactionRepositoryMock = new Mock<ITransactionRepository>();
+            accountRepositoryMock = new Mock<IAccountRepository>();
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                            .EnableSensitiveDataLogging()
+                            .Options;
+            ctx = new ApplicationDbContext(options);
+            sut = new WithdrawalController(transactionRepositoryMock.Object,accountRepositoryMock.Object);
+
+        }
+
+        [TestMethod]
+        public void AccountNotFound()
+        {
+            var viewModel = fixture.Create<NewWithdrawalViewModel>();
+            viewModel.AccountId = 998877665;
+            Assert.ThrowsException<InvalidDataException>(() => Withdrawal.Show(viewModel.AccountId));
         }
     }
 }
