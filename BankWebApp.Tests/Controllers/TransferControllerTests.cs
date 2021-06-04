01e1f5e78fe617021a8f0e969dcfbe963280d095
FadiAbd <fadishadow@hotmail.com>
2021-06-04 15:06:36 +0200

    TransferController

---
+++ BankWebApp.Tests/Controllers/TransferControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using BankWebbApp.Controllers;
+using BankWebbApp.Data;
+using BankWebbApp.Repository;
+using BankWebbApp.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankWebApp.Tests
+{
+    class TransferControllerTests : BaseTest
+    {
+
+        public class BaseTest
+        {
+            protected AutoFixture.Fixture fixture = new AutoFixture.Fixture();
+            private ApplicationDbContext ctx;
+
+        }
+
+        [TestClass]
+        public class TransferControllerTests : BaseTest
+        {
+            private TransferController transfer;
+            private IMock<ITransactionRepository> transactionRepositoryMock;
+            private IMock<IAccountRepository> accountRepositoryMock;
+            private ApplicationDbContext ctx;
+
+            public TransferControllerTests()
+            {
+                transactionRepositoryMock = new Mock<ITransactionRepository>();
+                accountRepositoryMock = new Mock<IAccountRepository>();
+
+                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .EnableSensitiveDataLogging()
+                    .Options;
+                ctx = new ApplicationDbContext(options);
+                transfer = new TransferController( accountRepositoryMock.Object ,transactionRepositoryMock.Object);
+
+
+            }
+
+            //[TestMethod]
+            //public void AccountIdNotFound()
+            //{
+            //    var viewModel = fixture.Create<NewTransferViewModel>();
+            //    viewModel.AccountId = 63336631;
+            //    Assert.ThrowsException<InvalidDataException>(() => transfer.Show(viewModel.AccountId));
+            //}
+
+        }
+    }
+}
--- BankWebApp.Tests/Controllers/UnitTest1.cs
+++ BankWebApp.Tests/Controllers/UnitTest1.cs
@@ -2,6 +2,7 @@ using BankWebbApp.Controllers;
 using BankWebbApp.Data;
 using BankWebbApp.Repository;
 using BankWebbApp.Services;
+using BankWebbApp.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -35,16 +36,49 @@ namespace BankWebApp.Tests
                             .EnableSensitiveDataLogging()
                             .Options;
             ctx = new ApplicationDbContext(options);
-            sut = new WithdrawalController(transactionRepositoryMock.Object,accountRepositoryMock.Object);
+            sut = new WithdrawalController( accountRepositoryMock.Object , transactionRepositoryMock.Object);
 
         }
 
         [TestMethod]
-        public void AccountNotFound()
+        public void Should_not_withdraw_more_than_the_Account_total_amount()
         {
-            var viewModel = fixture.Create<NewWithdrawalViewModel>();
-            viewModel.AccountId = 998877665;
+            var viewModel = new NewWithdrawalViewModel
+            {
+                AccountId = 1,
+                Amount = 111,
+                Operation = "Withdrawal in cash"
+
+            };
+            
             Assert.ThrowsException<InvalidDataException>(() => Withdrawal.Show(viewModel.AccountId));
         }
+
+        //[TestMethod]
+        //public void AccountNotFound()
+        //{
+        //    var viewModel = fixture.Create<NewWithdrawalViewModel>();
+        //    viewModel.AccountId = 998877665;
+        //    Assert.ThrowsException<InvalidDataException>(() => Withdrawal.Show(viewModel.AccountId));
+        //}
+    }
+
+    internal class NewWithdrawalViewModel
+    {
+        public int TransactionId { get; set; }
+
+       
+        public int AccountId { get; set; }
+
+       
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public string Operation { get; set; }
+     
+        public decimal Balance { get; set; }
+        public string Symbol { get; set; }
+        public string Bank { get; set; }
+        public string Account { get; set; }
     }
 }
